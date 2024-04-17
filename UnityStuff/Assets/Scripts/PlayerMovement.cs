8b8c765250cfe1aea7a33d06d90a30ec4392a536
Mikey St.John <132379177+Sansation00@users.noreply.github.com>
2024-04-17 11:43:16 -0400

    man
    no progress :(

--- UnityStuff/Assets/Scripts/PlayerMovement.cs
+++ UnityStuff/Assets/Scripts/PlayerMovement.cs
@@ -49,3 +49,13 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 }
+
+//Q is for using abilty
+//f light
+//g heavy
+// combos make bar go up
+// douge-doubble click keys/hold run
+// no jumping for big guy
+// big guy dmg drc atk inc
+// big guy is slower
+//
