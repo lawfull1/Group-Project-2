6021ddb66d60cbc2b09b6b576982e8ff90ef1ca9
Mikey St.John <132379177+Sansation00@users.noreply.github.com>
2024-04-24 11:42:24 -0400

    a
    a

--- UnityStuff/Assets/Scripts/PlayerMovement.cs
+++ UnityStuff/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -9,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
     private bool boogie = true;
     private float flowie = 10f;
     //Movement
-    private float speed;
+    private float speed = 10;
     public float jump;
     float moveVelocity;
     public Rigidbody2D rb;
@@ -35,25 +34,35 @@ public class PlayerMovement : MonoBehaviour
     public int prePuffyDamage;
     public int prePuffySpeed;
     private bool isPuffyPrint;
+    //Animations and stuff for dumies
+    public Animator ani;
+
     // Update is called once per frame
     void Update()
     {
         //Jumping
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W))
         {
+            ani.SetInteger("Animation_Control", 3);
             if (isGrounded == true && isPuffy == false && Jumps > 0)
             {
-                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + jump);
+                Jumps--;
             }
         }
         if (boogie == true)
         {
             flowie += speed;
         }
+        if(Input.GetKeyDown(KeyCode.A)||(Input.GetKeyDown(KeyCode.D)))
+        {
+            ani.SetInteger("Animation_Control", 1);
+        }
         moveVelocity = Input.GetAxisRaw("Horizontal") * speed;
-        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y*speed);
         if (Input.GetKeyDown(KeyCode.Q))
         {
+            ani.SetInteger("Animation_Control", 6);
             puffyActivated();
             print(puffyTime);
         }
@@ -61,17 +70,19 @@ public class PlayerMovement : MonoBehaviour
         hit();
         puffyTime += -Time.deltaTime;
         puffyCooldown += -Time.deltaTime;
+        ani.SetInteger("Animation_Control", 0);
     }
     void hit()
     {
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             damageDone = damage;
-
+            ani.SetInteger("Animation_Control", 4);
         }
         if (Input.GetKeyDown(KeyCode.Mouse2))
         {
             damageDone = damage * 2;
+            ani.SetInteger("Animation_Control", 8);
         }
     }
     //Check if Grounded
@@ -120,6 +131,7 @@ public class PlayerMovement : MonoBehaviour
             isPuffyPrint = true;
         }
         print(puffyCooldown);
+        ani.SetInteger("Animation_Control", 7);
     }
     private void managePuffy()
     {
@@ -129,8 +141,9 @@ public class PlayerMovement : MonoBehaviour
             {
                 isPuffy = false;
                 print("isPuffy is false (for debugging you monkey)");
+                puffyDeActivated();
             }
-            puffyDeActivated();
+            
         }
     }
 }
